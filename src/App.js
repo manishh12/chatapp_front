2ac134dc8c7adec8c72486fa67a1c065d6a4b3c0
manishh12 <manishdid360@gmail.com>
2023-07-08 00:21:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Register from "./pages/Register";
+import Chat from "./pages/chat";
+import Login from "./pages/Login";
+import SetAvatar from "./pages/SetAvatar";
+export default function App(){
+return <BrowserRouter>
+<Routes>
+  <Route path="/register" element={<Register/>} />
+  <Route path="/login" element={<Login/>} />
+  <Route path="/setAvatar" element={<SetAvatar/>} />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  <Route path="/" element={<Chat/>} />
+
+ 
+
+</Routes>
+</BrowserRouter>;
 }
-
-export default App;
---
+++ src/components/Messages.jsx
@@ -0,0 +1,11 @@
+import react from "react-router-dom";
+import styled from "styled-components";
+
+
+
+export default function Messages(){
+    return <Container>meassages</Container>
+}
+const Container=styled.div`
+height: 80%;
+`;
